d49fd01a1871dcab446b0afe5756a99878888110
Gordon McCreight <gordon@mccreight.com>
2014-03-31 21:57:02 -0700

    add just a little real testing

--- golang/code/code_test.go
+++ golang/code/code_test.go
@@ -3,8 +3,22 @@ package code
 import "testing"
 
 func TestInsertElement(t *testing.T) {
-  h := &Ele{"A", &Ele{"B", nil}}
-  h.insert("C")
+
+  h := &Ele{"A", nil}
+  if h.Data != "A" {
+    t.Errorf("data should be A but was %v", h.Data)
+  }
+
+  h.insert("B")
+  if h.Next.Data != "B" {
+    t.Errorf("data should be B but was %v", h.Next.Data)
+  }
+
+  h.Next.insert("C")
+  if h.Next.Next.Data != "C" {
+    t.Errorf("data should be C but was %v", h.Next.Next.Data)
+  }
+
 }
 
 //ogfileid:31
