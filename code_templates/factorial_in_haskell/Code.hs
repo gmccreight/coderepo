58b16afb01f4a3eeaa024f230ca5e4e7cb020dd4
Gordon McCreight <gordon@mccreight.com>
2014-04-05 19:25:01 -0700

    Add haskell runner and factorial

---
+++ code_templates/factorial_in_haskell/Code.hs
@@ -0,0 +1,5 @@
+module Code ( fac ) where
+
+fac n = if n == 0 then 1 else n * fac (n-1)
+
+--cffileid:1
---
+++ code_templates/factorial_in_haskell/unittests.hs
@@ -0,0 +1,13 @@
+import Code
+
+import Test.HUnit
+
+test0 = TestCase (assertEqual "factorial for 0" (1) (fac 0))
+test1 = TestCase (assertEqual "factorial for 1" (1) (fac 1))
+test2 = TestCase (assertEqual "factorial for 2" (2) (fac 2))
+test3 = TestCase (assertEqual "factorial for 3" (6) (fac 3))
+test4 = TestCase (assertEqual "factorial for 4" (24) (fac 4))
+test5 = TestCase (assertEqual "factorial for 5" (120) (fac 5))
+
+main = runTestTT $ TestList [test0, test1, test2, test3, test4, test5]
+--cffileid:2
