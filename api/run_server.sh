b16941ca10414abcba8099946336bfdbacb15249
Gordon McCreight <gordon@mccreight.com>
2014-04-12 16:08:28 -0700

    Add some inline documentation

--- api/run_server.sh
+++ api/run_server.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+echo "note: the server is actually accessable at port 8080"
+echo "so, for example, try http://localhost:8080/?template=stack_in_c"
 vagrant ssh -c "cd /vagrant/api/app; DEBUG=codefluent-api ./bin/www"
