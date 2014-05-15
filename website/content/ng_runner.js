21e18f2fc85c1761bc04fc52de6b9e86685aa76f
Gordon McCreight <gordon@mccreight.com>
2014-05-15 10:38:13 -0700

    Add ability to change what is run in the text areas

--- website/content/ng_runner.js
+++ website/content/ng_runner.js
@@ -3,6 +3,14 @@ window.codeFluentApp = angular.module('CodeFluentApp', []);
 window.codeFluentApp.controller("RunnerCtrl", function ($scope, $http) {
 
   $scope.error = "";
+  $scope.files = undefined;
+  $scope.currentFile = undefined;
+  $scope.isRunning = false;
+
+  $scope.init = function() {
+    $scope.files = $scope.templateFor().files;
+    $scope.currentFile = $scope.files[0].name;
+  };
 
   $scope.hasError = function() {
     return $scope.error != "";
@@ -16,10 +24,16 @@ window.codeFluentApp.controller("RunnerCtrl", function ($scope, $http) {
     $scope.didPass = didPass;
   };
 
+  $scope.setIsRunning = function(isRunning) {
+    $scope.isRunning = isRunning;
+  };
+
   $scope.runCode = function() {
     $scope.setError("");
     $scope.setDidPass(false);
+    $scope.setIsRunning(true);
     var successCallback = function(data, status, headers, config) {
+      $scope.setIsRunning(false);
       if (data.error) {
         $scope.setError(data.error);
       }
@@ -27,13 +41,16 @@ window.codeFluentApp.controller("RunnerCtrl", function ($scope, $http) {
         if (data.did_pass) {
           $scope.setDidPass(true);
         }
+        else {
+          $scope.setError(data.stdout + data.stderr);
+        }
       }
     };
     $http.post('http://localhost:8080/', $scope.getDataToPost()).success(successCallback);
   };
 
   $scope.getDataToPost = function() {
-    return $scope.templateFor("clojure");
+    return {files: $scope.files};
   };
 
   $scope.templateFor = function(name) {
