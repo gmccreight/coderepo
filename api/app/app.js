3b99a5f1d67ab7f346cd9c2c3116dcbc1c019dae
Gordon McCreight <gordon@mccreight.com>
2014-05-15 07:35:52 -0700

    Allow cross-domain posts to API

--- api/app/app.js
+++ api/app/app.js
@@ -10,6 +10,14 @@ var users = require('./routes/users');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,6 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(allowCrossDomain);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
