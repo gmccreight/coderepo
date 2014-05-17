db56a55d2266058721fde8cf1a5026ceb4742644
Gordon McCreight <gordon@mccreight.com>
2014-05-17 13:38:23 -0700

    Remove two unused files

--- website/output/main_runner.js
+++
@@ -1,6 +0,0 @@
-angular.module('CodeFluentApp', [])
-.controller("MainRunnerCtrl", function ($scope) {
-
-  $scope.getData = function() {return "hello";};
- 
-});
--- website/output/runner.js
+++
@@ -1,6 +0,0 @@
-angular.module('CodeFluentApp', [])
-.controller("RunnerCtrl", function ($scope) {
-
-  $scope.getData = function() {return "hello";};
- 
-});
