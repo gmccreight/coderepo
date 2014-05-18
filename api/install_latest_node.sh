2afb2cf1831ef30e800a2f219ff4e126e53af6c8
Gordon McCreight <gordon@mccreight.com>
2014-05-17 23:40:00 -0700

    ensure that the node installer has g++ already

--- api/install_latest_node.sh
+++ api/install_latest_node.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-wget http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz
+# g++ is a prerequisite for compiling node
+sudo apt-get install g++ -y
+
+wget http://nodejs.org/dist/v0.10.28/node-v0.10.28.tar.gz
 tar -xvzf node-v*.tar.gz
 rm node-v*.tar.gz
 cd node-v*
