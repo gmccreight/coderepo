70f3d793cdc6496fd860621cb26e853585dd991f
Gordon McCreight <gordon@mccreight.com>
2014-05-18 23:35:33 -0700

    Add make installation

--- runner_api/install_latest_node.sh
+++ runner_api/install_latest_node.sh
@@ -3,6 +3,9 @@
 # g++ is a prerequisite for compiling node
 sudo apt-get install g++ -y
 
+# make isn't pre-installed on the EC2 version of Ubuntu
+sudo apt-get install make -y
+
 wget http://nodejs.org/dist/v0.10.28/node-v0.10.28.tar.gz
 tar -xvzf node-v*.tar.gz
 rm node-v*.tar.gz
