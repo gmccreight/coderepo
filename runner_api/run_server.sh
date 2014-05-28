ee0a300321387d855e62fff7e10becceee3a432c
Gordon McCreight <gordon@mccreight.com>
2014-05-28 09:42:16 -0700

    remove vagrant ssh stuff

--- runner_api/run_server.sh
+++ runner_api/run_server.sh
@@ -2,4 +2,5 @@
 
 echo "note: the server is actually accessable at port 8080"
 echo "try running the curl_test.sh file: ./runner_api/curl_test.sh"
-vagrant ssh -c "cd /vagrant/runner_api/app; DEBUG=codefluent-api ./bin/www"
+
+cd /vagrant/runner_api/app; DEBUG=codefluent-api ./bin/www
